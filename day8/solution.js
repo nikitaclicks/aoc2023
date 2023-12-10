54872c9530fb8d6f8b94624c021d99317c5ec379
Nikita Dmitriev <nikitadmitry@gmail.com>
2023-12-10 23:02:48 +0100

    d8

---
+++ day8/solution.js
@@ -0,0 +1,126 @@
+function p1(input) {
+  const rows = input.split("\n");
+
+  const instructions = rows[0];
+
+  const nodes = new Map();
+
+  for (let i = 2; i < rows.length; ++i) {
+    const [from, to] = rows[i].split('=').map(x => x.trim());
+    const directions = to.substring(1, to.length - 1).split(', ');
+    nodes.set(from, directions);
+  }
+
+  let steps = 0;
+
+  for (let pos = 'AAA', step = 0; pos !== 'ZZZ'; ++step) {
+    if (step === instructions.length) {
+      step = 0;
+    }
+    const instruction = instructions[step];
+
+    const node = nodes.get(pos);
+    pos = instruction === 'L' ? node[0] : node[1];
+
+    steps++;
+  }
+
+  return steps;
+}
+
+module.exports.p1 = p1;
+
+function p2(input) {
+  const rows = input.split("\n");
+
+  const instructions = rows[0];
+
+  const nodes = new Map();
+
+  const startNodes = [];
+
+  for (let i = 2; i < rows.length; ++i) {
+    const [from, to] = rows[i].split('=').map(x => x.trim());
+    const directions = to.substring(1, to.length - 1).split(', ');
+    nodes.set(from, directions);
+
+    if (from[2] === 'A') {
+      startNodes.push(from);
+    }
+  }
+
+  const zLoops = [];
+
+  for (let position of startNodes) {
+    const zPositions = [];
+    let steps = 1;
+
+    for (let step = 0; ; ++step) {
+      if (step === instructions.length) {
+        step = 0;
+      }
+      const instruction = instructions[step];
+
+      const node = nodes.get(position);
+      position = instruction === 'L' ? node[0] : node[1];
+
+      if (position[2] === 'Z') {
+        const posKey = `${position}_${step + 1}`;
+        if (zPositions.length && zPositions[0].key === posKey && zPositions[0].steps % 2 === steps % 2) {
+          break;
+        } else {
+          zPositions.push({ key: posKey, steps });
+        }
+      }
+
+      ++steps;
+    }
+
+    zLoops.push(zPositions);
+  }
+
+  const nums = zLoops.map(x => x[0].steps);
+
+  const lcm = leastCommonMultiple(nums);
+
+  return lcm;
+
+  // const positions = startNodes;
+
+  // let steps = 0;
+
+  // for (let step = 0; !positions.every(p => p[2] === 'Z'); ++step) {
+  //   if (step === instructions.length) {
+  //     step = 0;
+  //   }
+  //   const instruction = instructions[step];
+
+  //   for (let i = 0; i < positions.length; i++) {
+  //     const node = nodes.get(positions[i]);
+  //     positions[i] = instruction === 'L' ? node[0] : node[1];
+  //   }
+
+  //   steps++;
+  // }
+
+  // return steps;
+}
+
+function leastCommonMultiple(nums) {
+  function gcd(a, b) {
+      return !b ? a : gcd(b, a % b);
+  }
+
+  function lcm(a, b) {
+      return (a * b) / gcd(a, b);   
+  }
+
+  var multiple = nums[0];
+  nums.forEach(function(n) {
+      multiple = lcm(multiple, n);
+  });
+
+  return multiple;
+}
+
+module.exports.p2 = p2;
---
+++ day8/test.js
@@ -0,0 +1,60 @@
+const { p1, p2 } = require("./solution");
+const input = require("./input");
+
+const testCase = `RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`;
+
+const testCase2 = `LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`;
+
+test.each([
+  ["test", testCase, 2, []],
+  ["test2", testCase2, 6, []],
+  ['input', input, 17141, []]
+])("p1 works for %s", (name, input, expectation, bads) => {
+  const actual = p1(input);
+
+  if (bads?.length) {
+    for (const bad of bads) {
+      expect(actual).not.toBe(bad);
+    }
+  }
+
+  expect(actual).toBe(expectation);
+});
+
+const testCaseP2 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+test.each([
+  ["test", testCaseP2, 6, []],
+  ['input', input, 10818234074807, []]
+])("p2 works for %s", (name, input, expectation, bads) => {
+  const actual = p2(input);
+
+  if (bads?.length) {
+    for (const bad of bads) {
+      expect(actual).not.toBe(bad);
+    }
+  }
+
+  expect(actual).toBe(expectation);
+});
